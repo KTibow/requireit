307b7f75d20216a9577a45446cc8a4710c5ad293
kendell <KTibow@users.noreply.github.com>
2020-04-29 08:51:25 -0700

    Update requireit.py

--- requireit.py
+++ requireit.py
@@ -1,29 +1,45 @@
-#This is requireit, a way to simplify user experience by automatically installing libraries.
-#Find out about it at https://github.com/KTibow/requireit
-from pip._internal import main as pipmain
-# pipit is just a way to run pip
-pipit = lambda theargs: pipmain(theargs.split(" "))
-# just a custom exception
+#https://github.com/KTibow/requireit
+from pip._internal import main as pip
+import __main__
 class VersionError(Exception):
   pass
+class InstallError(Exception):
+  pass
 def requireit(libs):
-  """Libs is a list of libraries, like ["requests", "ffsend"]. Returns True if everything went right and False elsewise.
-  If there's an error importing one library, it'll keep on importing. 
-  The other nice thing about requireit is that packages can be dynamic. You don't need to manually type import a whole bunch of times.
-  requireit is not a replacement for requirements.txt; it's just a way to streamline user experience by not making them open a command prompt.
-  The reason that you can't import requireit is that it'll only import libraries in its own file, not your code."""
-  worked = True
   for lib in libs:
     try:
       import importlib
     except ImportError:
-    	raise VersionError("Please upgrade your Python interpreter.")
+      raise VersionError('Please upgrade Python')
     try:
-    	globals()[lib] = importlib.import_module(lib)
-    except ModuleNotFoundError as e:
+      if __name__ == '__main__':
+        if isinstance(lib, str):
+          globals()[lib] = importlib.import_module(lib)
+        else:
+          globals()[lib[0]] = importlib.import_module(lib[0])
+      else:
+        if isinstance(lib, str):
+          __main__.__dict__[lib] = importlib.import_module(lib)
+        else:
+          __main__.__dict__[0] = importlib.import_module(lib[0])
+    except ModuleNotFoundError:
       try:
-        pipit("install "+lib)
-        globals()[lib] = importlib.import_module(lib)
-      except Exception as f:
-        worked = False
-  return worked
+        if isinstance(lib, str):
+          n(['install', lib])
+        else:
+          n(['install', lib[1])
+        if __name__ == '__main__':
+          if isinstance(lib, str):
+            globals()[lib] = importlib.import_module(lib)
+          else:
+            globals()[lib[0]] = importlib.import_module(lib[0])
+        else:
+          if isinstance(lib, str):
+            __main__.__dict__[lib] = importlib.import_module(lib)
+          else:
+            __main__.__dict__[0] = importlib.import_module(lib[0])
+      except Exception:
+        if isinstance(lib, str):
+          raise InstallError("Couldn't auto-install " + lib)
+        else:
+          raise InstallError("Couldn't auto-install " + lib[0])
