24b40a79f53791007d6ced4992b815f6ce7b092e
kendell <KTibow@users.noreply.github.com>
2020-04-29 10:46:07 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+def test_docstring():
+  import requireit
+  assert len(requireit.requireit.__doc__) > 0
+  del requireit
+def test_valid():
+  import requireit
+  import requireitmini
+  assert hasattr(requireit, requireit)
+  assert hasattr(requireitmini, requireit)
+  del requireit
+  del requireitmini
+  exec(open("requireitmini-inline.py", "r"))
+  assert "requireit" in locals()
+  del A
+  del VersionError
+  del InstallError
+  del E
+  del F
+  del requireit
