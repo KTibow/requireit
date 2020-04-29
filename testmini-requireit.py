c5b9a4bd5c8c121580c6656c10bd9e417b3a4269
kendell <KTibow@users.noreply.github.com>
2020-04-29 11:40:54 -0700

    Update testmini-requireit.py

--- testmini-requireit.py
+++ testmini-requireit.py
@@ -1,6 +1,6 @@
 import requireitmini
-assert onedrivesdk not in globals()
-assert test0408 not in globals()
+assert "onedrivesdk" not in globals()
+assert "test0408" not in globals()
 requireitmini.requireit([["onedrivesdk", "git+https://github.com/OneDrive/onedrive-sdk-python.git"], "test0408"])
 assert isinstance(onedrivesdk, object)
 assert isinstance(test0408, object)
