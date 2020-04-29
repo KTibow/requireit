afca18e7b4c678b298c1518cddb75e63168d2915
kendell <KTibow@users.noreply.github.com>
2020-04-28 18:34:25 -0700

    Update requireitmini.py

--- requireitmini.py
+++ requireitmini.py
@@ -2,21 +2,21 @@
 from pip._internal import main as n
 q = lambda a: n(a.split(" "))
 class VersionError(Exception):
-	pass
+  pass
 def requireit(m):
-	w = True
-	for l in m:
-		try:
-			import importlib
-		except ImportError:
-			w = False
-			raise VersionError("Please upgrade Python")
-		try:
-			globals()[l] = importlib.import_module(l)
-		except ModuleNotFoundError as e:
-			try:
-				q("install "+l)
-				globals()[l] = importlib.import_module(l)
-			except Exception as f:
-				w = False
-	return w
+  w = True
+  for l in m:
+    try:
+      import importlib
+    except ImportError:
+      w = False
+      raise VersionError("Please upgrade Python")
+    try:
+      globals()[l] = importlib.import_module(l)
+    except ModuleNotFoundError:
+      try:
+        q("install "+l)
+        globals()[l] = importlib.import_module(l)
+      except Exception:
+        w = False
+        return w
