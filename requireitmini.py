8c9ad140a72506644a0826d68d00e902136fecd2
kendell <KTibow@users.noreply.github.com>
2020-04-29 09:08:15 -0700

    Update requireitmini.py

--- requireitmini.py
+++ requireitmini.py
@@ -1,11 +1,32 @@
-#https://github.com/KTibow/requireit
-from pip._internal import main as n
-class VersionError(Exception): pass
-def requireit(m):
-  for l in m:
-    try: import importlib
-    except ImportError: raise VersionError("Please upgrade Python")
-    try: globals()[l if isinstance(l, str) else l[0]]=importlib.import_module(l if isinstance(l, str) else l[0])
+# https://github.com/KTibow/requireit
+from pip._internal import main as A
+import __main__
+class VersionError(Exception): 0
+class InstallError(Exception): 0
+D = __name__ == '__main__'
+E = "Couldn't auto-install "
+def requireit(B):
+  for C in B:
+    F = isinstance(C, str)
+    try: import importlib as il
+    except ImportError: raise VersionError('Please upgrade Python')
+    try:
+      if D:
+        if F: globals()[C]=il.import_module(C)
+        else: globals()[C[0]]=il.import_module(C[0])
+      else:
+        if F: __main__.__dict__[C]=il.import_module(C)
+        else: __main__.__dict__[C[0]]=il.import_module(C[0])
     except ModuleNotFoundError:
-      try: n(["install",l if isinstance(l, str) else l[1]]); globals()[l if isinstance(l, str) else l[0]]=importlib.import_module(l if isinstance(l, str) else l[0])
-      except Exception: return False
+      try:
+        if F: A(['install',C])
+        else: A(['install',C[1])
+        if D:
+          if F: globals()[C]=il.import_module(C)
+          else: globals()[C[0]]=il.import_module(C[0])
+        else:
+          if F: __main__.__dict__[C]=il.import_module(C)
+          else: __main__.__dict__[C[0]]=il.import_module(C[0])
+      except Exception:
+        if F: raise InstallError(E+lib)
+        else: raise InstallError(E+lib[0])
