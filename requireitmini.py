a6df8eb72fb20192eda8e2c4cba22c10b3664dd9
kendell <KTibow@users.noreply.github.com>
2020-04-29 09:18:08 -0700

    Update requireitmini.py

--- requireitmini.py
+++ requireitmini.py
@@ -3,30 +3,21 @@ from pip._internal import main as A
 import __main__
 class VersionError(Exception): 0
 class InstallError(Exception): 0
-D = __name__ == '__main__'
-E = "Couldn't auto-install "
+D=__name__=='__main__';E="Couldn't auto-install "
 def requireit(B):
   for C in B:
-    F = isinstance(C, str)
-    try: import importlib as il
-    except ImportError: raise VersionError('Please upgrade Python')
+    J=C if isinstance(C, str) else C[0]
+    try:from importlib import import_module as il
+    except ImportError:raise VersionError('Please upgrade Python')
     try:
       if D:
-        if F: globals()[C]=il.import_module(C)
-        else: globals()[C[0]]=il.import_module(C[0])
+        globals()[J]=il(J)
       else:
-        if F: __main__.__dict__[C]=il.import_module(C)
-        else: __main__.__dict__[C[0]]=il.import_module(C[0])
+        __main__.__dict__[J]=il(J)
     except ModuleNotFoundError:
       try:
-        if F: A(['install',C])
+        if isinstance(C, str): A(['install',C])
         else: A(['install',C[1]])
-        if D:
-          if F: globals()[C]=il.import_module(C)
-          else: globals()[C[0]]=il.import_module(C[0])
-        else:
-          if F: __main__.__dict__[C]=il.import_module(C)
-          else: __main__.__dict__[C[0]]=il.import_module(C[0])
-      except Exception:
-        if F: raise InstallError(E+lib)
-        else: raise InstallError(E+lib[0])
+        if D: globals()[J]=il(J)
+        else: __main__.__dict__[J]=il(J)
+      except Exception: raise InstallError(E+J)
