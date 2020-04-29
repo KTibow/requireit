1110435a58f90c2b875599692130eb819d40eb55
kendell <KTibow@users.noreply.github.com>
2020-04-28 18:30:17 -0700

    Update requireitmini.py

--- requireitmini.py
+++ requireitmini.py
@@ -1,21 +1,22 @@
 #https://github.com/KTibow/requireit
-from pip._internal import main as m
-pipit = lambda a: pipmain(a.split(" "))
+from pip._internal import main as n
+q = lambda a: n(a.split(" "))
 class VersionError(Exception):
 	pass
 def requireit(m):
-  w = True
-  for l in m:
-    try:
-      import importlib
-    except ImportError:
-    	raise VersionError("Please upgrade your Python interpreter.")
-    try:
-    	globals()[l] = importlib.import_module(l)
-    except ModuleNotFoundError as e:
-      try:
-        pipit("install "+l)
-        globals()[l] = importlib.import_module(l)
-      except Exception as f:
-        w = False
-  return w
+	w = True
+	for l in m:
+		try:
+			import importlib
+		except ImportError:
+			w = False
+			raise VersionError("Please upgrade Python")
+		try:
+			globals()[l] = importlib.import_module(l)
+		except ModuleNotFoundError as e:
+			try:
+				q("install "+l)
+				globals()[l] = importlib.import_module(l)
+			except Exception as f:
+				w = False
+	return w
