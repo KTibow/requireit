367adbc335da06a1ba91f86e9c8821e57e39fc56
kendell <KTibow@users.noreply.github.com>
2020-04-29 10:10:32 -0700

    Create requireitmini-inline.py

---
+++ requireitmini-inline.py
@@ -0,0 +1,16 @@
+# https://github.com/KTibow/requireit
+from pip._internal import main as A
+import __main__
+class VersionError(Exception):0
+class InstallError(Exception):0
+E="Couldn't auto-install ";F='install'
+def requireit(B):
+	for C in B:
+		J=C if isinstance(C,str)else C[0]
+		try:from importlib import import_module as L
+		except ImportError:raise VersionError('Please upgrade Python')
+		try:
+			globals()[J]=L(J)
+		except ModuleNotFoundError:
+			try:A([F,C]) if isinstance(C,str)else A([F,C[1]]);globals()[J]=L(J)
+			except Exception:raise InstallError(E+J)
