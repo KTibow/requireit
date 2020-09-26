26fd70100c4973e001b3c531b9db133c44bc054d
Kendell R <KTibow@users.noreply.github.com>
2020-09-26 07:32:52 -0700

    Update requireitmini-inline.py

--- requireitmini-inline.py
+++ requireitmini-inline.py
@@ -1,13 +1,13 @@
-from pip._internal import main as A;import sys # This is requireit: https://github.com/KTibow/requireit
+from subprocess import check_output as A;import sys#git.io/JUVEE
 class VersionError(Exception):0
 class InstallError(Exception):0
-E="Couldn't auto-install ";F='install'
+E="Couldn't auto-install ";F=[sys.executable,"-m","pip","install"]
 def requireit(B):
 	for C in B:
 		J=C if isinstance(C,str)else C[0]
 		try:from importlib import import_module as L
-		except ImportError:raise VersionError('Please upgrade Python')
+		except ImportError:raise VersionError("⬆🐍")
 		try: globals()[J]=L(J)
 		except ModuleNotFoundError if sys.version_info.minor > 5 else ImportError:
-			try:A([F,C]) if isinstance(C,str)else A([F,C[1]]);globals()[J]=L(J)
+			try:A(F+[C]) if isinstance(C,str)else A(F+[C[1]]);globals()[J]=L(J)
 			except Exception:raise InstallError(E+J)
